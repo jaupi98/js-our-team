6c64f55e916cbf24f5e68a5f7f99d7f0a552e7c7
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-06-02 20:27:55 +0200

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const team = [
+    {
+        'name': 'Wayne Barnett',
+        'role': 'Founder & Ceo',
+        'image':'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'name': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'image':'angela-caroll-chief-editor.jpg'
+    },
+    {
+        'name': 'walter Gordon',
+        'role': 'Office Manager',
+        'image':'walter-gordon-office-manager.jpg'
+    },
+    {
+        'name': 'Angela Lopez',
+        'role': 'social  Media Manager',
+        'image':'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'name': 'Scott Estrada',
+        'role': 'Doveloper',
+        'image':'scott-estrada-developer.jpg'
+    },
+    {
+        'name': 'Barbara Ramos',
+        'role': 'Grafic Designer',
+        'image':'barbara-ramos-graphic-designer.jpg'
+    }
+];
+// MILESTONE 1
+for(let i=0; i<team.length; i++){
+    console.log(team[i].name);
+    console.log(team[i].role);
+    console.log(team[i].image);
+}
+
